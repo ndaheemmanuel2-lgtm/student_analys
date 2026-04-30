f4e8b2e6f573a5c9aaa4df9604f56d2b0cd8f70f
ndahe  emmanuel <ndaheemmanuel2-lgtm>
2026-04-30 07:40:17 +0100

    fix port

--- app.py
+++ app.py
@@ -126,6 +126,6 @@ def analyse():
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
+        port=int(os.environ.get("PORT", 8080)),
         debug=False
     )
